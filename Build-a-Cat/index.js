4ab2da31db49f31e44eea75f73b94be03fb41513
Gregory A Lundberg <gregory_lundberg@gmx.com>
2022-03-06 19:53:18 -0600

    Clean up
    Isolate doChange from the global context. Make things more readable and consistent.

--- Build-a-Cat/index.js
+++ Build-a-Cat/index.js
@@ -1,42 +1,42 @@
-function doChange()
-{
-    var selected_head = document.getElementById('head').options[document.getElementById('head').selectedIndex].text;
-    var fur = document.getElementById('fur').options[document.getElementById('fur').selectedIndex].text + ".png";
-    var eyecolor = document.getElementById('eyecolor').options[document.getElementById('eyecolor').selectedIndex].text.toLowerCase() + ".png";
-    var eyeshape = document.getElementById('eyeshape').options[document.getElementById('eyeshape').selectedIndex].text;
-    var pupil = document.getElementById('pupil').options[document.getElementById('pupil').selectedIndex].text;
-    var ear = document.getElementById('ear').options[document.getElementById('ear').selectedIndex].text;
-    var whiskercolor = document.getElementById('whiskercolor').options[document.getElementById('whiskercolor').selectedIndex].text + ".png";
-    var whiskershape = document.getElementById('whiskershape').options[document.getElementById('whiskershape').selectedIndex].text;
-    var head = "";
-    if (selected_head === "Floofy") head = "Floofy_head/";
-    document.getElementById("shadowlayer").src = "https://kittycats.ws/online/images/ears/" + head + ear + "_SHADOW.png";
-    document.getElementById("furlayer").src = "https://kittycats.ws/online/images/ears/" + head + ear + "/" + fur;
-    document.getElementById("eyeslayer").src = "https://kittycats.ws/online/images/eyes/" + eyeshape + "_" + pupil + "/" + eyecolor;
-    document.getElementById("whiskerslayer").src = "https://kittycats.ws/online/images/whiskers/" + whiskershape + "/" + whiskercolor;
-    if (fur.substring(0,11) === "Confetti - ")
-        document.getElementById('confetti').className = "show";
-    else
-        document.getElementById('confetti').className = '';
-}
+(function(){
+    function doChange() {
+        var selected_head = document.getElementById('head'        ).options[document.getElementById('head'        ).selectedIndex].text
+        var fur           = document.getElementById('fur'         ).options[document.getElementById('fur'         ).selectedIndex].text
+        var eyecolor      = document.getElementById('eyecolor'    ).options[document.getElementById('eyecolor'    ).selectedIndex].text
+        var eyeshape      = document.getElementById('eyeshape'    ).options[document.getElementById('eyeshape'    ).selectedIndex].text
+        var pupil         = document.getElementById('pupil'       ).options[document.getElementById('pupil'       ).selectedIndex].text
+        var ear           = document.getElementById('ear'         ).options[document.getElementById('ear'         ).selectedIndex].text
+        var whiskercolor  = document.getElementById('whiskercolor').options[document.getElementById('whiskercolor').selectedIndex].text
+        var whiskershape  = document.getElementById('whiskershape').options[document.getElementById('whiskershape').selectedIndex].text
 
-document.addEventListener('readystatechange', () => {
-    if (document.readyState === 'complete') {
-        document.getElementById('head').addEventListener('change', doChange)
-        document.getElementById('fur').addEventListener('change', doChange)
-        document.getElementById('eyecolor').addEventListener('change', doChange)
-        document.getElementById('eyeshape').addEventListener('change', doChange)
-        document.getElementById('pupil').addEventListener('change', doChange)
-        document.getElementById('ear').addEventListener('change', doChange)
-        document.getElementById('whiskercolor').addEventListener('change', doChange)
-        document.getElementById('whiskershape').addEventListener('change', doChange)
+        var head = (selected_head === 'Floofy') ? 'Floofy_head/' : ''
+
+        document.getElementById('shadowlayer'  ).src = 'https://kittycats.ws/online/images/ears/' + head + ear + '_SHADOW.png';
+        document.getElementById('furlayer'     ).src = 'https://kittycats.ws/online/images/ears/' + head + ear + '/' + fur + '.png';
+        document.getElementById('eyeslayer'    ).src = 'https://kittycats.ws/online/images/eyes/' + eyeshape + '_' + pupil + '/' + eyecolor.toLowerCase() + '.png';
+        document.getElementById('whiskerslayer').src = 'https://kittycats.ws/online/images/whiskers/' + whiskershape + '/' + whiskercolor + '.png';
+
+        document.getElementById('confetti').className = (fur.substring(0,11) === 'Confetti - ') ? 'show' : '';
     }
-})
+
+    document.addEventListener('readystatechange', () => {
+        if (document.readyState === 'complete') {
+            document.getElementById('head'        ).addEventListener('change', doChange)
+            document.getElementById('fur'         ).addEventListener('change', doChange)
+            document.getElementById('eyecolor'    ).addEventListener('change', doChange)
+            document.getElementById('eyeshape'    ).addEventListener('change', doChange)
+            document.getElementById('pupil'       ).addEventListener('change', doChange)
+            document.getElementById('ear'         ).addEventListener('change', doChange)
+            document.getElementById('whiskercolor').addEventListener('change', doChange)
+            document.getElementById('whiskershape').addEventListener('change', doChange)
+        }
+    })
+}())
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service_worker.js', {
         scope: '/Build-a-Cat/'
     }).catch(function(error) {
-        console.log('Registration failed with ' + error);
+        console.log('Service worker registration failed with ' + error);
     });
 }
